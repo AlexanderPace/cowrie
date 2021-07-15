75951d875bd078a4f9f8697a2eef76378c20392a
AlexanderPace <pacealex@ecs.vuw.ac.nz>
2021-07-15 15:33:47 +1200

    Remove smart caching skeleton code, add command replay skeleton code

--- src/cowrie/core/fspersistence.py
+++ src/cowrie/core/fspersistence.py
@@ -1,19 +1,29 @@
 """
-This module enables Cowrie to provide filesystem persistence.
-A user logging on more than once will see the same filesystem as they did the previous time they logged on.
+This module enables Cowrie to provide simulated filesystem persistence.
+A user logging on more than once will see almost the same filesystem as they did the previous time they logged on.
+This is achieved by replaying filesystem-related commands upon user return.
 
 (c) Alexander Pace, 2021
 """
 
-# Two functions, to be triggered at logon and logoff
-# Logon checks for an existing cache record JSON. If there is one, it applies the changes described, drawing on cached files.
-# It then pours the full VM filesystem structure into a JSON file with hashes on each item.
-# Logoff hashes the whole filesystem again, and compares it to the JSON file created at logon. Any differences are cached. The JSON generated at logon is discarded.
-# The cached files are recorded in another JSON file.
+def record_fs_commands():
+    """
+    Scans log for current user and copies out all filesystem-related commands to a file 'fs_cmds'.
+    If this file is already present, it is appended.
+    """
 
-def logon():
-    # Use os.walk() as per this StackOverflow answer: https://stackoverflow.com/questions/29634781/finding-md5-of-files-recursively-in-directory-in-python
+def save_command_history():
+    """
+    Saves the command history of the current user to a file 'cnd_hist'.
+    If this file is already present, it is appended.
+    """
 
-def logoff():
+def replay_fs_commands():
+    """
+    Executes, in order, any previous filesystem-related commands executed by this user.
+    """
 
-
+def restore_command_history():
+    """
+    Restores a user's commands from a previous session to the command history file.
+    """
